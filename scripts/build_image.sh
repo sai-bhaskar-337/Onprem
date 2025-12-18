e35a132a6a43d94099237d5728f6d65e9ccf0647
saibhaskar337 <saibhaskar337@gmail.com>
2025-12-18 16:11:24 +0530

    replace the new pro

--- scripts/build_image.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-IMAGE="$1"   # e.g. yourdockerhubusername/myapp
-TAG="$2"     # e.g. gitsha
-docker build -t "${IMAGE}:${TAG}" .
-docker tag "${IMAGE}:${TAG}" "${IMAGE}:latest"
